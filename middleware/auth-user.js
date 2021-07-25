3fabd7ac35de6f47e80a4fcfdd291394e115c145
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-25 20:34:50 +1000

    User authentication middleware

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -6,59 +6,64 @@ const { User, Course } = require('../models');
 exports.authenticateUser = async (req, res, next) => {
 
     let message;
-    console.log(req.headers.authorization);
-    // Parse the user's credentials from the Authorization header.
-    const headers = req.headers.authorization.substring(6);
-    const header_arr = headers.split(":");
-    let credentials = {};
-    credentials.email = header_arr[0];
-    credentials.pass = header_arr[1];
+    if (req.headers.authorization) {
 
-    // If the user's credentials are available...
-    // Attempt to retrieve the user from the data store
-    // by their username (i.e. the user's "key"
-    // from the Authorization header).
-    if (credentials) {
-        const user = await User.findOne({
-            where:{
-                emailAddress: credentials.email
-            },
-            include: [
-                {
-                    model: Course,
-                    as: 'teacher',
-                    attributes: ['title', 'description', 'estimatedTime', 'materialsNeeded']
+        // Parse the user's credentials from the Authorization header.
+        const headers = req.headers.authorization.substring(6);
+        const header_arr = headers.split(":");
+        let credentials = {};
+        credentials.email = header_arr[0];
+        credentials.pass = header_arr[1];
+
+
+        // If the user's credentials are available...
+        // Attempt to retrieve the user from the data store
+        // by their username (i.e. the user's "key"
+        // from the Authorization header).
+        if (credentials) {
+            const user = await User.findOne({
+                where: {
+                    emailAddress: credentials.email
+                },
+                include: [
+                    {
+                        model: Course,
+                        as: 'teacher',
+                        attributes: ['title', 'description', 'estimatedTime', 'materialsNeeded']
+                    }
+                ],
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
                 }
-            ],
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            }
-        })
+            })
 
-        // If a user was successfully retrieved from the data store...
-        // Use the bcrypt npm package to compare the user's password
-        // (from the Authorization header) to the user's password
-        // that was retrieved from the data store.
-        if (user){
-            const authenticated = bcrypt.compareSync(credentials.pass, user.password);
+            // If a user was successfully retrieved from the data store...
+            // Use the bcrypt npm package to compare the user's password
+            // (from the Authorization header) to the user's password
+            // that was retrieved from the data store.
+            if (user) {
+                const authenticated = bcrypt.compareSync(credentials.pass, user.password);
 
-            // If the passwords match...
-            // Store the retrieved user object on the request object
-            // so any middleware functions that follow this middleware function
-            // will have access to the user's information.
-            if (authenticated){
-                req.currentUser = user;
+                // If the passwords match...
+                // Store the retrieved user object on the request object
+                // so any middleware functions that follow this middleware function
+                // will have access to the user's information.
+                if (authenticated) {
+                    req.currentUser = user;
+                } else {
+                    message = `Authentication failure for email: ${user.emailAddress}`;
+                }
             } else {
-                message = `Authentication failure for email: ${user.emailAddress}`;
+                message = `User not found for email: ${credentials.email}`;
             }
         } else {
-            message = `User not found for email: ${credentials.email}`;
+            message = "Auth header not found";
         }
     } else {
-        message = "Auth header not found";
-    }
+    message = "Auth header not found";
+}
 
-    if (message){
+    if (message) {
         // If user authentication failed...
         // Return a response with a 401 Unauthorized HTTP status code.
         console.warn(message);
